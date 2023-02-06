f48f6df69cfc1d404ed34a5c56a2c236ab9c16f5
artur-redfab <artur@redfab.ru>
2023-02-06 14:48:20 +0300

    add env

--- models/database.py
+++ models/database.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from pydantic import PostgresDsn
+from decouple import config
+
 
 SQLALCHEMY_DATABASE_URI = PostgresDsn.build(
     scheme="postgresql",
-    user="postgres",
-    password="cosmos1234",
-    host="localhost",
-    port="5433",
-    path=f"/{'redfab_vk_python' or ''}",
+    user=config('PG_USER'),
+    password=config('PG_PASS'),
+    host=config('PG_SERVER'),
+    port=config('PG_PORT'),
+    path=f"/{config('PG_DB') or ''}",
 )
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI,
