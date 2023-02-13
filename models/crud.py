fccd7eef06102d72ed7b4e72fd5d17c4cfc90bed
Кондратенко Алексей Александрович <kond@omega.local>
2023-02-13 14:37:20 +0300

    Creating crud methods for makers table

--- models/crud.py
+++ models/crud.py
@@ -132,3 +132,43 @@ def get_features_by_user_id(db: Session, user_id: int):
     return db_user
 
 
+# методы для работы с моделью Makers
+def create_maker(db: Session, maker: schemas.MakerName):
+    db_maker = models.Makers(
+        name=maker.name
+    )
+    db.add(db_maker)
+    db.commit()
+    db.refresh(db_maker)
+    return db_maker
+
+
+def change_maker(db: Session, maker: schemas.MakerName, maker_id: int):
+    db_maker = db.query(models.Makers).filter(models.Makers.id == maker_id).first()
+    db_maker.name = maker.name
+    db.commit()
+
+
+def hide_maker(db: Session, maker_id: int):
+    db_maker = db.query(models.Makers).filter(models.Makers.id == maker_id).first()
+    db_maker.markingDeletion = True
+    db.commit()
+
+
+def show_maker(db:Session, maker_id: int):
+    db_maker = db.query(models.Makers).filter(models.Makers.id == maker_id).first()
+    db_maker.markingDeletion = False
+    db.commit()
+
+
+def get_makers(db: Session):
+    db_makers = db.query(models.Makers).all()
+    return db_makers
+
+
+def get_features(db: Session, maker_id: int):
+    db_maker = db.query(models.Makers).filter(models.Makers.id == maker_id).first()
+    return db_maker
+
+
+
