734204cea5417ce029d7dab9ac241e2e6d39cb08
Кондратенко Алексей Александрович <kond@omega.local>
2023-02-13 14:50:52 +0300

    Filling endpoints /create

--- models/schemas.py
+++ models/schemas.py
@@ -64,9 +64,15 @@ class UserFeatures(BaseModel):
 class MakerName(BaseModel):
     name: str
 
+    class Config:
+        orm_mode = True
+
 
 class MakerId(BaseModel):
     id: str
+
+    class Config:
+        orm_mode = True
 
 
 class MakerList(MakerId):
--- routers/makers.py
+++ routers/makers.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Cookie, Response, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models import crud, schemas
+from models.database import get_db
 
 router = APIRouter(
     prefix='/hs/makers',
@@ -7,14 +9,14 @@ router = APIRouter(
 )
 
 
-@router.post("/create")
-def create_maker():
-    pass
+@router.post("/create", response_model=schemas.MakerId)
+def create_maker(maker: schemas.MakerName, db: Session = Depends(get_db)):
+    return crud.create_maker(db=db, maker=maker)
 
 
-@router.put("/change/{id}")
-def change_maker():
-    pass
+@router.put("/change/{id}", response_status=200)
+def change_maker(maker: schemas.MakerId, db: Session = Depends(get_db)):
+
 
 
 @router.delete("/delete/{id}")
