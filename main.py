8262004bcd23b848b974c8fb01a68f0dbb8e94d3
artur-redfab <artur@redfab.ru>
2023-02-20 17:03:21 +0300

    RedFabMES-453

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from components.vacuumSystem import models
 from db.database import engine
-from components.colors.routers import router as colors_routers
+# from components.colors.routers import router as colors_routers
 from components.users.routers import router as user_routers
 from components.materials.routers import router as materials_routers
 from components.projects.routers import router as projects_routers
@@ -20,7 +20,7 @@ async def pong():
 
 
 # color api
-app.include_router(colors_routers)
+# app.include_router(colors_routers)
 # users api
 app.include_router(user_routers)
 # project api
--- tests/makers/test_makers.py
+++ tests/makers/test_makers.py
@@ -1,4 +1,23 @@
-import pytest
-from components.makers.crud import get_maker_by_id
-def test_get_maker_by_id():
-assert get_maker_by_id(1) == {""}
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_makers_list():
+    response = client.get("/hs/makers/list")
+    assert response.status_code == 200
+    assert response.json() == [{
+        "markingDeletion": False,
+        "id": 1,
+        "name": "new_name"
+    }]
+
+
+def test_makers_change():
+    response = client.put("/hs/makers/change/1",
+                          json={"name": "new_name"}
+                          )
+    assert response.status_code == 200
+    assert response.json() == "Maker changed successfully!"
