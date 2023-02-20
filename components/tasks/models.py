2e9ce4689faffb1225bb4e0b021077ea37589b14
Кондратенко Алексей Александрович <kond@omega.local>
2023-02-20 13:26:49 +0300

    Creating tasks model

--- components/tasks/models.py
+++ components/tasks/models.py
@@ -1,6 +1,25 @@
 import datetime
-from sqlalchemy import DateTime
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Interval,DateTime, Numeric
 from db.database import Base
 
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    idProject = Column(Integer, ForeignKey('projects.id'), nullable=False)
+    idPriority = Column(Integer, ForeignKey('priorities.id'), nullable=False)
+    numberCopies = Column(Integer, nullable=False)
+    planPrintTime = Column(Interval, nullable=False)
+    factPrintTime = Column(Interval, nullable=False)
+    idOperGroup = Column(Integer, nullable=False) # TODO: ForeignKey(""), nullable=False)
+    twoExtrPrint = Column(Boolean, nullable=False)
+    idBasicMaterial = Column(Integer, ForeignKey('materials.id'), nullable=False)
+    idBasicColor = Column(Integer, ForeignKey('colors.id'), nullable=False)
+    idSupportMaterial = Column(Integer, ForeignKey('materials.id'), nullable=False)
+    idSupportColor = Column(Integer, ForeignKey('colors.id'), nullable=False)
+    volume = Column(Numeric, nullable=False)
+    markingDeletion = Column(Boolean, nullable=False, default=False)
+    update = Column(DateTime, nullable=False, default=datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=3), name='MSK')))
 
