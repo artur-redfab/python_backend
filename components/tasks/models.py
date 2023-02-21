d4eee2819b50ef8be46ebdfe63b06866f358a8d4
Кондратенко Алексей Александрович <kond@omega.local>
2023-02-21 12:28:16 +0300

    Adding tasks models

--- components/tasks/models.py
+++ components/tasks/models.py
@@ -23,3 +23,32 @@ class Tasks(Base):
     markingDeletion = Column(Boolean, nullable=False, default=False)
     update = Column(DateTime, nullable=False, default=datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=3), name='MSK')))
 
+
+class TaskStatuses(Base):
+    __tablename__ = "taskStatuses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+
+class TaskStatusHistory(Base):
+    __tablename__ = "taskStatusHistory"
+
+    id = Column(Integer, primary_key=True, index=True)
+    period = Column(DateTime, nullable=False, default=datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=3), name='MSK')))
+    idPrinter = Column(Integer, nullable=False) # TODO: ForeignKey("printers.id")
+    idTask = Column(Integer, ForeignKey('tasks.id'), nullable=False)
+    idTaskStatus = Column(Integer, ForeignKey('taskStatuses.id'), nullable=False)
+
+
+class TasksCopies(Base):
+    __tablename__ = "tasksCopies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    idTask = Column(Integer, ForeignKey('tasks.id'), nullable=False)
+    numCopy = Column(Integer, nullable=False)
+    printTime = Column(Integer, nullable=False)
+    startAt = Column(DateTime)
+    idTaskStatus = Column(Integer, ForeignKey('taskStatuses.id'))
+    markingDeletion = Column(Boolean, nullable=False, default=False)
+
