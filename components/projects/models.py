1f78a4df2c4903be9ce4f84f2677d10e8cb0f23e
Кондратенко Алексей Александрович <kond@omega.local>
2023-02-27 16:17:43 +0300

    Creating new models

--- components/projects/models.py
+++ components/projects/models.py
@@ -21,3 +21,18 @@ class Projects(Base):
     comment = Column(String)
     markingDeletion = Column(Boolean, nullable=False, default=False)
 
+
+class ProjectStatuses(Base):
+    __tablename__ = "projectStatuses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+
+class ProjectStatusHistory(Base):
+    __tablename__ = "projectStatusHistory"
+
+    id = Column(Integer, primary_key=True, index=True)
+    period = Column(DateTime, nullable=False, default=datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=3), name='MSK')))
+    idProject = Column(Integer, ForeignKey('projects.id'), nullable=False)
+    idProjectStatus = Column(Integer, ForeignKey('projectStatuses.id'), nullable=False)
